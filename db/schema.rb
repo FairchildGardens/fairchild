925f7d7772cee34683d2543827eebc38e202eaf8
Jose Casanova <josescasanova@gmail.com>
2016-03-05 20:14:39 -0500

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306003410) do
+ActiveRecord::Schema.define(version: 20160305230530) do
 
   create_table "hunt_users", force: :cascade do |t|
     t.integer  "user_id"
