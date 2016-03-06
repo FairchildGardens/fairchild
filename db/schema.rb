2fd0eef891550d0697f240f563d88ed62b6c0b7d
Todd Metheny <todd.metheny@gmail.com>
2016-03-05 22:15:09 -0500

    added admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306005020) do
+ActiveRecord::Schema.define(version: 20160306030709) do
 
   create_table "hunt_users", force: :cascade do |t|
     t.integer  "user_id"
@@ -60,9 +60,10 @@ ActiveRecord::Schema.define(version: 20160306005020) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "role"
+    t.boolean  "admin",      default: false
   end
 
 end
