abb5043ba66c427e3ce56ffb8785330787bbc9af
Jose Casanova <josescasanova@gmail.com>
2016-03-06 00:08:44 -0500

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,41 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file .env file.
 
-plants = Plant.first(50)
+hunts = []
+6.times  do |i|
+  hunt = Hunt.create(
+    name: "Hunt #{i}",
+    image: 'http://i.imgur.com/CveMZTU.jpg'
+  )
+  hunts << hunt
+end
 
-plants.each do |plant|
-  puts 'calling wiki'
-  wiki = Task.search_wikipedia(plant.botanical_name)
+plants = Plant.all
 
-  puts "Wiki success: #{wiki}"
-  if wiki[:success]
-    puts "Plant Name: #{plant.botanical_name}"
-    task = Task.where(name: plant.botanical_name.titlecase)
+hunts.each do |hunt|
+  plant_count = 0
+  plants.each do |plant|
+    if plant_count < 10
+      puts 'calling wiki'
+      task = Task.where(name: plant.botanical_name.titlecase)
 
-    if task.blank?
-      Task.create(name: plant.botanical_name.titlecase, description: wiki[:excerpt], image: wiki[:image])
+      if task.blank?
+        wiki = Task.search_wikipedia(plant.botanical_name)
+
+        puts "Wiki success: #{wiki}"
+        if wiki[:success]
+          puts "Plant Name: #{plant.botanical_name}"
+
+          Task.create(name: plant.botanical_name.titlecase, description: wiki[:excerpt], image: wiki[:image], hunt_id: hunt.id)
+          plant_count += 1
+
+        end
+      end
+
     end
-
   end
 end
+
 
 # Hunt.create(
 #   name: "#{plant.botanical_name}",
